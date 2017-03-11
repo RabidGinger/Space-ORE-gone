86de689577b4d0fbedb31d401e511004bc078739
knweber1996 <katienoelleweber@gmail.com>
2017-03-11 12:00:39 -0700

    Character
    Character file

---
+++ Space ORE Gone/src/Main/Character.java
@@ -0,0 +1,13 @@
+
+public class Character {
+	
+	protected String name;
+	protected int gender;
+	protected String role;
+
+	public Character (String name, int gender, String role)	{
+		this.name = name;
+		this.gender = gender;
+		this.role = role;
+	} //test
+}
