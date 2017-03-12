7fc285eeb0a71380eb3569d10faac5deac1fb7ff
Mrkyl <kyle.kingston@asu.edu>
2017-03-12 00:58:47 -0700

    Add files via upload

---
+++ src/dev/codemore/tilegame/Launcher.java
@@ -0,0 +1,12 @@
+package dev.codemore.tilegame;
+
+import dev.codemore.tilegame.display.Display;
+
+public class Launcher {
+
+public static void main (String[] args){
+	Game game = new Game("Ore Gone Trail!", 300, 300);
+	game.start();
+	
+}
+}
